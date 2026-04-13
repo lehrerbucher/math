cdab7447ab74085d91ecd63ffc447429c462e6c8
Patrick Bucher <patrick.bucher@sluz.ch>
2026-04-13 09:00:59 +0200

    TDD: table-driven tests

--- gcd_test.ts
+++ gcd_test.ts
@@ -1,73 +1,19 @@
 import { assertEquals } from "@std/assert/equals";
 import { gcdBruteForce, gcdEuclid } from "./gcd.ts";
 
-Deno.test("gcd(1, 1) is 1", () => {
-  // Arrange
-  const a = 1;
-  const b = 1;
-  const expected = 1;
+const gcdTests = [
+  { a: 1, b: 1, gcd: 1 },
+  { a: 2, b: 3, gcd: 1 },
+  { a: 12, b: 9, gcd: 3 },
+  { a: 12, b: 8, gcd: 4 },
+  { a: 18, b: 27, gcd: 9 },
+  { a: 81, b: 54, gcd: 27 },
+  { a: 81, b: 55, gcd: 1 },
+];
 
-  // Act
-  const actual: number = gcdBruteForce(a, b);
-
-  // Assert
-  assertEquals(actual, expected);
+Deno.test("gcd implementations: Brute Force and Euclid", () => {
+  for (const { a, b, gcd } of gcdTests) {
+    assertEquals(gcdBruteForce(a, b), gcd);
+    assertEquals(gcdEuclid(a, b), gcd);
+  }
 });
-
-Deno.test("gcd(3, 7) is 1", () => {
-  // Arrange
-  const a = 3;
-  const b = 7;
-  const expected = 1;
-
-  // Act
-  const actual = gcdBruteForce(a, b);
-
-  // Assert
-  assertEquals(actual, expected);
-});
-
-Deno.test("gcd(12, 9) is 3", () => {
-  // Arrange
-  const a = 12;
-  const b = 9;
-  const expected = 3;
-
-  // Act
-  const actual = gcdBruteForce(a, b);
-
-  // Assert
-  assertEquals(actual, expected);
-});
-
-Deno.test("gcd(81, 54) is 27", () => {
-  assertEquals(gcdBruteForce(81, 54), 27);
-});
-
-Deno.test("gcd(1, 1) is 1", () => {
-  // Arrange
-  const a = 1;
-  const b = 1;
-  const expected = 1;
-
-  // Act
-  const actual = gcdEuclid(a, b);
-
-  // Assert
-  assertEquals(actual, expected);
-});
-
-Deno.test("gcd(12, 9) is 3", () => {
-  assertEquals(gcdEuclid(12, 9), 3);
-});
-
-Deno.test("gcd(81, 54) is 27", () => {
-  assertEquals(gcdEuclid(12, 9), 3);
-});
-
-
-Deno.test("gcd(198, 98) is 2", () => {
-  assertEquals(gcdEuclid(12, 9), 3);
-});
-
-
